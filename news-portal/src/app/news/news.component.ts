b392c90921e661308819ccab2eef5e55416f5979
Mahmoud Mohamed <mahmoud.mo.alaa1999@gmail.com>
2024-11-10 14:34:41 +0200

    PUSHING STATIC NEWS

---
+++ news-portal/src/app/news/news.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'news-latest',
+  templateUrl: './news.component.html',
+  styleUrl: './news.component.scss'
+})
+export class NewsComponent {
+  title:string='Latest News';
+
+
+}
+
---
+++ news-portal/src/app/topics/topics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'news-topics',
+  templateUrl: './topics.component.html',
+  styleUrl: './topics.component.scss'
+})
+export class TopicsComponent {
+  title:string='Hot Topics';
+}
